32f2e40e9413f59024b7f128adc8df4e460b2b8d
Will Ockelmann-Wagner <will@whentheresawill.net>
2017-11-25 13:11:48 -0800

    ramdomize land ratings

--- lib/ants/ants/ant_move.ex
+++ lib/ants/ants/ant_move.ex
@@ -21,10 +21,20 @@ defmodule Ants.Ants.AntMove do
   end
 
   @spec rate_tile(Tile.t) :: integer
-  def rate_tile(%Food{}), do: 3
-  def rate_tile(%Land{}), do: 2
+  def rate_tile(%Food{}), do: 5
+  def rate_tile(%Land{}), do: random_rating(3)
   def rate_tile(%Home{}), do: 1
   def rate_tile(%Rock{}), do: 0
+
+  @spec randon_rating(integer) :: float
+  defp random_rating(rating) do
+    rating + random_decimal()
+  end
+
+  @spec random_decimal() :: float
+  defp random_decimal() do
+    Enum.random(-100..100) / 100
+  end
 
   defp get_index(tuple), do: elem(tuple, 1)
 
